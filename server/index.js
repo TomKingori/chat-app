2f32487b139b496de437a912b2c55c3a76e25cac
Tom Kingori <87486793+TomKingori@users.noreply.github.com>
2024-06-25 09:43:57 +0300

    chat activity detection

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require("express");
 const { Server } = require("socket.io");
-const path = require('path')
-require('dotenv').config()
+const path = require("path");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3400
+const PORT = process.env.PORT || 3400;
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-const expressServer = app.listen(PORT, ()=>{
-  console.log(`listening on port ${PORT}`)
-})
+const expressServer = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
 
 const io = new Server(expressServer, {
   cors: {
@@ -25,8 +25,31 @@ const io = new Server(expressServer, {
 io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
 
+  // Upon connection - only to user
+  socket.emit("message", "Welcome to Chat App!");
+
+  // Upon connection - to all others
+  socket.broadcast.emit(
+    "message",
+    `User ${socket.id.substring(0, 5)} connected`
+  );
+
+  // Listening for a message event
   socket.on("message", (data) => {
     console.log(data);
     io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
   });
+
+  // When user disconnects - to all others
+  socket.on('disconnect', ()=>{
+    socket.broadcast.emit(
+      "message",
+      `User ${socket.id.substring(0, 5)} disconnected`
+    );
+  })
+
+  //Listen for activity
+  socket.on('activity', (name)=> {
+    socket.broadcast.emit('activity', name)
+  })
 });
